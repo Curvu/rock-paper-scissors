32f7efad31966fa13eabae52e1a3466dbb138a93
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-07-17 12:21:03 +0100

    Function for the computer choice

--- main.js
+++ main.js
@@ -6,5 +6,13 @@ btns.forEach(btn => {
     btn.addEventListener('click', (e) => {
         let choice = e.target.id
         console.log(choice);
-    })
+        let computer = getComputerChoice();
+        console.log(computer)
+        console.log('-----')
+    });
 });
+
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)]
+}
