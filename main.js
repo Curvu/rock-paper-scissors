d56fbbb5774e67a2894d4be7778acd27e13fe88d
Curvu <rodrigues.filipealexandre2003@gmail.com>
2022-07-18 12:35:53 +0100

    function to calculate who is winning

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const btns = document.querySelectorAll('.btns');
 
-let cp = parseInt(document.getElementById('cp').textContent);
-let yp = parseInt(document.getElementById('yp').textContent);
+
+const compScore = document.getElementById('cp');
+const yScore = document.getElementById('yp');
+let cp = parseInt(compScore.textContent);
+let yp = parseInt(yScore.textContent);
+
+const winner = document.getElementById('winner');
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
@@ -9,8 +14,7 @@ btns.forEach((btn) => {
         console.log(choice);
         let computer = getComputerChoice();
         console.log(computer);
-        calculateWinner(choice, computer)
-        console.log('-----');
+        calculateWinner(choice, computer);
     });
 });
 
@@ -20,13 +24,33 @@ function getComputerChoice() {
 }
 
 function calculateWinner(choice, computer) {
-    if(choice === computer) console.log('Draw')
+    if(choice === computer) winner.innerText = 'Draw!'
     else if(choice === 'rock' && computer === 'paper' || choice === 'paper' && computer === 'scissors' || choice === 'scissors' && computer === 'rock') {
         cp++;
-        console.log('Computer won!');
+        winner.innerText = 'Computer Won the point!'
     }
     else {
         yp++;
-        console.log('You won!');
+        winner.innerText = 'You won the point!'
+    }
+    renderScore();
+}
+
+function renderScore() {
+    document.getElementById('cp').textContent = cp;
+    document.getElementById('yp').textContent = yp;
+    whosLoosing();
+}
+
+function whosLoosing() {
+    if (cp > yp) {
+        compScore.style.color = 'green';
+        yScore.style.color = 'red';
+    } else if (yp > cp) {
+        yScore.style.color = 'green';
+        compScore.style.color = 'red';
+    } else {
+        compScore.style.color = 'black';
+        yScore.style.color = 'black';
     }
 }
